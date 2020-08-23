2294373e8b86b3c027cad9877781d674777f091e
kerm11 <1837278602@qq.com>
2020-08-23 14:42:10 +0800

    0823

---
+++ C83-S3-Pzm-blog/src/main/java/com/yc/C83S3Pzmblog/C83S3PzmBlogApplication.java
@@ -0,0 +1,13 @@
+package com.yc.C83S3Pzmblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C83S3PzmBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C83S3PzmBlogApplication.class, args);
+	}
+
+}
---
+++ C83-S3-Pzm-blog/src/test/java/com/yc/C83S3Pzmblog/C83S3PzmBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yc.C83S3Pzmblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C83S3PzmBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
