1d9144111ac9be161e53d1de1596c8e48a6f3908
kerm11 <1837278602@qq.com>
2020-08-29 22:58:19 +0800

    灿然书屋第一天

---
+++ C83-S3-Pzm-crbook-zuul/src/main/java/com/yc/crbook/CrZuulApplication.java
@@ -0,0 +1,32 @@
+package com.yc.crbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@EnableEurekaClient
+@EnableCircuitBreaker
+@SpringBootApplication
+public class CrZuulApplication  implements WebMvcConfigurer{
+
+
+		public static void main(String[] args) {
+			SpringApplication.run(CrZuulApplication .class, args);
+		}
+
+		/**
+		 *    定义 RestTemplate  Bean
+		 */
+		@LoadBalanced
+		@Bean
+		public RestTemplate getRestTemplate() {		
+			return new RestTemplate();		
+		}
+}
