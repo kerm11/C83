181bb9f42ef48d43396ee9250256167aadf28391
kerm11 <1837278602@qq.com>
2020-08-30 13:30:07 +0800

    0829灿然1

--- C83-S3-Pzm-crbook-eureka/src/main/java/com/yc/crbook/CrEurekaApplication.java
+++ C83-S3-Pzm-crbook-eureka/src/main/java/com/yc/crbook/CrEurekaApplication.java
@@ -3,15 +3,14 @@ package com.yc.crbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-@EnableEurekaClient
 
+@EnableEurekaServer
 @SpringBootApplication
 public class CrEurekaApplication  implements WebMvcConfigurer{
 
